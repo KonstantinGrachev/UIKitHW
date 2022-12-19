607515147bbbd1b2adf904c09548e3588158e91f
KonstantinGrachev <pibadubadidad@gmail.com>
2022-12-19 06:52:07 +0300

    Initial Commit

---
+++ UIKitHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitHW
+//
+//  Created by Konstantin Gracheff on 19.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
